aa21be17cbacdd4a14bf2487e0526f807121bc9b
khitish kumar pradhan <36028384+khitishpradhan@users.noreply.github.com>
2021-06-02 21:57:59 +0530

    Delete server directory

--- server/routes/crops.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { getCrops } from "../controllers/crops.js";
-
-const router = express.Router();
-
-router.get("/:name", getCrops);
-
-export default router;
